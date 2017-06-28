aceccd5bfdc1e9aaa3022796d7f4ee47b0b59800
wisamjs <wisamzaghal@gmail.com>
2017-06-28 16:15:50 -0400

    use updated deadlift name

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ export const FIVE_SETS_OF_FIVE = '5x5';
 export const SET_FORMATS = [SET_OF_FIVE, SET_OF_ANY, FIVE_SETS_OF_FIVE];
 
 export const SQUAT = 'Low Bar Squat';
-export const DEADLIFT = 'Barbell Deadlift';
+export const DEADLIFT = 'Deadlift';
 export const BENCH = 'Bench Press';
 export const ROW = 'Bent Over Barbell Row';
 export const OHP = 'Overhead Press';
